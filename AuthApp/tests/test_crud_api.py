506a7d3d3c849cd42f8dd7c202d6d548eb21595d
tthen17 <adeleamst@gmail.com>
2024-10-27 11:11:49 +0300

    Delete AuthApp/tests directory

--- AuthApp/tests/test_crud_api.py
+++
@@ -1,8 +0,0 @@
-def test_register(test_client, user_payload):
-    response = test_client.post("/register", json=user_payload)
-    assert response.status_code == 200
-    assert response.json() == {"message": "user created successfully"}
-
-# def test_login(test_client, user_payload):
-#     response = test_client.get("/login")
-#     assert response.status_code == 200
